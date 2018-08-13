cd1ccac6eab4f42f1fdeb6f03394dcaa0c4c9e8e
Craig Mulligan <hobochildster@gmail.com>
2018-08-13 23:01:10 +0100

    add colors and symbols

--- src/bar.ts
+++ src/bar.ts
@@ -1,4 +1,59 @@
-const { bgWhite } = require('chalk')
+import * as chalk from 'chalk'
+
+const SYMBOLS = {
+  Empty: '',
+  Full: '█',
+  SevenEighths: '█',
+  ThreeQuarters: '█',
+  FiveEighths: '█',
+  Half: '▌',
+  ThreeEighths: '▌',
+  Quarter: '▌',
+  Eighth: '▌', 
+}
+
+const getSymbolNormal = value => {
+  if (value <= 0) {
+    return SYMBOLS.Empty;
+  } else if (value <= 1 / 8) {
+    return SYMBOLS.Eighth;
+  } else if (value <= 1 / 4) {
+    return SYMBOLS.Quarter;
+  } else if (value <= 3 / 8) {
+    return SYMBOLS.ThreeEighths;
+  } else if (value <= 1 / 2) {
+    return SYMBOLS.Half;
+  } else if (value <= 5 / 8) {
+    return SYMBOLS.FiveEighths;
+  } else if (value <= 3 / 4) {
+    return SYMBOLS.ThreeQuarters;
+  } else if (value <= 7 / 8) {
+    return SYMBOLS.SevenEighths;
+  } else {
+    return SYMBOLS.Full;
+  }
+}
+
+// win32 systems support only half-block symbol and no corners 😭
+if (process.platform === 'win32') {
+  Object.assign(SYMBOLS, {
+    LeftCorner: '',
+    RightCorner: '',
+    Full: '█',
+    SevenEighths: '█',
+    ThreeQuarters: '█',
+    FiveEighths: '█',
+    Half: '▌',
+    ThreeEighths: '▌',
+    Quarter: '▌',
+    Eighth: '▌',
+  });
+}
+
+const splitNumber = (value = 0) => {
+  const [int, rest = '0'] = value.toString().split('.');
+  return [parseInt(int, 10), parseInt(rest, 10) / Math.pow(10, rest.length)];
+};
 
 export default class Bar {
   constructor({ total }) {
@@ -6,16 +61,29 @@ export default class Bar {
     this.maxWidth = process.stdout.columns - 30
   }
 
-  draw({ duration, startOffset, path }) {
+  draw({ duration, startOffset, path, color }) {
     const width = duration / this.total
     const offSet = startOffset / this.total
 
-    const fillLength = (width * this.maxWidth).toFixed(0)
-    const fillOffset = (offSet * this.maxWidth).toFixed(0)
+    const fillLength = width * this.maxWidth
+    const fillOffset = offSet * this.maxWidth
 
-    const filledBar = this.getBar(fillLength, ' ', bgWhite)
+    const [int, rest] = splitNumber(fillLength)
+    
+    // shamelessly taken from 
+    // https://github.com/gribnoysup/wunderbar/blob/b42565b9af92addc82c53bea6e4512339822578d/lib/bars.js
+    const string =
+    int < 0
+      ? ''
+      : getSymbolNormal(int).repeat(int) +
+        // We are handling zero value as a special case
+        // to print at least something on the screen
+        getSymbolNormal(int === 0 && rest === 0 ? 0.001 : rest);
 
-    process.stdout.cursorTo(parseInt(fillOffset))
+    // const filledBar = this.getBar(fillLength, ' ', chalk.hex(color))
+    const filledBar = chalk.hex(color)(string)
+
+    process.stdout.cursorTo(Math.floor(fillOffset))
     process.stdout.write(
       `${filledBar} - ${path.join('.')} - ${this.printDuration(duration)}\n`,
     )
@@ -28,16 +96,5 @@ export default class Bar {
       return `${ms} ms`
     }
     return `${microSeconds} µs`
-  }
-
-  getBar(length, char, color = a => a) {
-    let str = ' '
-    if (length === 0) {
-      str += char
-    }
-    for (let i = 0; i < length; i++) {
-      str += char
-    }
-    return color(str)
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 const { bgWhite } = require('chalk')
-const data = require('./data.json')
+// const data = require('./data.json')
+const data = require('../data.json')
 import Bar from './bar'
+const randomColor = require('randomcolor')
 
 const getTotal = ({ extensions }) => {
   const { duration } = extensions.tracing
@@ -12,14 +14,15 @@ const printData = () => {
   process.stdout.cursorTo(0)
   const total = getTotal(data)
   const bar = new Bar({ total })
+  const tracing = data.extensions.tracing
 
   const {
     execution: { resolvers },
-  } = data.extensions.tracing
+  } = tracing
 
-  bar.draw({ duration: total, startOffset: 0, path: ['total'] })
+  bar.draw({ duration: total, startOffset: 0, path: ['total'], color: randomColor() })
 
-  resolvers.map(r => bar.draw(r))
+  resolvers.map(r => bar.draw({...r, color:  randomColor()}))
 }
 
 printData()
